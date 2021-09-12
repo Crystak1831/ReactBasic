87da1ab5aae88ac3f06a2c1ac8fd1b2e395058ea
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-09-12 18:34:47 -0400

    add setInterval

--- src/basics/stateAndLifecycle/stateAndLifecycle.js
+++ src/basics/stateAndLifecycle/stateAndLifecycle.js
@@ -26,14 +26,54 @@ class StateAndLifecycle extends React.Component {
 
         {/* 2. State + Lifecycle */}
         <Clock />
+        <hr />
+        {/* 3.  */}
       </div>
     );
   }
 }
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    };
+  }
+
+  componentDidMount() {
+    // 设置定时器和ajax call
+    this.timer = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString()
+      });
+    }, 1000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // seconds is even, re-render ui, seconds is odd, pause ui
+    const second = nextState.time.split(" ")[0].split(":")[2];
+    // console.log(second)
+    if (second % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state.time) {
+      console.log("state has changed");
+    }
+  }
+
+  //记得的取消
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
-    return <div>{new Date().toLocaleTimeString()}</div>;
+    return <div>{this.state.time}</div>;
   }
 }
 export default StateAndLifecycle;
